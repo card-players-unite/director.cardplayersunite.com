e873566189b8571f130d409ee05f687d83ce098c
Software Shinobi <the.software.shinobi@gmail.com>
2024-01-12 14:48:08 -0500

    automated terminal push

--- scripting/tournaments/load-tournaments-metrics.js
+++ scripting/tournaments/load-tournaments-metrics.js
@@ -25,7 +25,7 @@ function displayPricingTickData() {
 
 		success: function (responseData, status, jqXHR) {
 
-            alert("api got a call!");
+        //    alert("api got a call!");
 
 	        console.log("responseData / ", responseData);
 
@@ -33,21 +33,30 @@ function displayPricingTickData() {
 
             for (var i = 0 ; i < responseData.length; i++) {
 	        
-                trHTML += "<tr scope='row'>";
+                trHTML += '<tr class="text-gray-700 dark:text-gray-400">';
 
-                trHTML += '<td class="">' + responseData[i].id + '</td>';
+                trHTML += '<td class="px-4 py-3 text-sm">' + responseData[i].id + '</td>';
 
-                trHTML += '<td class="">' + responseData[i].name + '</td>';
+                trHTML += '<td class="px-4 py-3 text-sm">' + responseData[i].name + '</td>';
 
-                trHTML += '<td class="">' + responseData[i].description + '</td>';	
+                trHTML += '<td class="px-4 py-3 text-sm">' + responseData[i].description + '</td>';	
 
-                trHTML += '<td class="">' + responseData[i].rental_price + '</td>';
+                trHTML += '<td class="px-4 py-3 text-sm">' + responseData[i].rental_price + '</td>';
 
-                trHTML += '<td class="">' + responseData[i].rented_user_id + '</td>';
+        //        trHTML += '<td class="px-4 py-3 text-sm">' + responseData[i].rented_user_id + '</td>';
 
+                     trHTML += ' <td class="px-4 py-3 text-xs">';
+                    trHTML += '    <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">';
+
+//                    trHTML += '      Approved';
+
+                    trHTML += responseData[i].rented_user_id
+
+                   trHTML += '     </span>';
+                  trHTML += '    </td>';
             }
 
-            $('#activity-display-table  > tbody').html(trHTML); 
+            $('#tournament-all-listing > tbody').html(trHTML); 
 
 		},
 
